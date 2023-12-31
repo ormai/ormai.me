898c8cf7e2ec43b86749e9bfb36f453fce95d2fa
Mario D'Andrea <goffredo2004@gmail.com>
2023-12-31 16:12:02 +0100

    Color scheme always follows system

--- src/theme.js
+++ src/theme.js
@@ -1,37 +1,12 @@
-const themeSelector = document.getElementById("theme")
-const isSystemDark = window.matchMedia("(prefers-color-scheme: dark)");
+const system = window.matchMedia("(prefers-color-scheme: dark)");
 
-setTheme(getCurrentTheme()); // on load
-
-function setTheme(theme) {
-  localStorage.setItem("theme", theme);
-  let system = ""; // for the icon
-
-  let newTheme = theme;
-  if (theme === "system") {
-    newTheme = isSystemDark.matches ? "dark" : "light";
-    system = "system-"; // for the icon
-  }
-
-  document.getElementById("themeIcon") // icon
-    .setAttribute("src", `./static/theme-${system}${newTheme}.svg`);
-
-  document.documentElement.setAttribute("data-theme", newTheme);
+function setTheme() {
+  document.documentElement.setAttribute(
+    "data-theme",
+    system.matches ? "dark" : "light",
+  );
 }
 
-function getCurrentTheme() {
-  let value = localStorage.getItem("theme");
-  if (!value) value = "system"; // default to system
-  return value
-}
+setTheme(); // on load
 
-isSystemDark.addEventListener("change", () => {
-  const currentTheme = getCurrentTheme();
-  if (currentTheme === "system")
-    setTheme(currentTheme);
-})
-
-themeSelector.addEventListener("change", (event) => {
-  setTheme(event.target.value);
-});
-
+system.addEventListener("change", () => setTheme()); // on change
