20dc501cd24a29a18a4bd4b325406f95b5ac5adc
Mario D'Andrea <goffredo2004@gmail.com>
2025-07-20 10:26:24 +0200

    update website domain name

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 # Should probably do some preprocessing like minifying.
 
-rsync --archive --delete --info=progress src/ server:/var/www/ormai.dev
+rsync --archive --delete --info=progress src/ server:/var/www/ormai.me
