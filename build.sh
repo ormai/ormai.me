170feffdbc6fe4740a73cbbe7d3b9db936082242
Mario D'Andrea <goffredo2004@gmail.com>
2025-09-07 09:41:22 +0200

    add 'serve' subcommand

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@
 set -e
 
 deploy() {
-  cp -rf ./src ./target
-  minify -ri target
+  cp -Rf ./src ./target
+  # https://github.com/tdewolff/minify
+  minify --recursive --inplace target
   rsync --archive --delete --info=progress target/ server:/var/www/ormai.me
 }
 
 case "$1" in
 deploy) deploy ;;
+serve)
+  cd src
+  xdg-open http://localhost:8000
+  python -m http.server
+  ;;
 clean) rm -r target ;;
 *) echo "unkown subcommand $1" ;;
 esac
